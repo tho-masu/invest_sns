3f0498b4626a80bfa29c580c05901e1f1c70dede
qcq1677 <qcq1677@N01-0255.quick.local>
2019-09-04 13:20:50 +0900

    css,javascript追加

--- sugeno_js/read_more.js
+++ sugeno_js/read_more.js
@@ -2,6 +2,16 @@ $(function(){
 	$('.readmore').click(function(){
 		//$(this).hide();
 		$(this).parent('.main').toggleClass('onhidden');
-		$(this)
+		var text = $(this).children('span').text();
+		if(text == "続きを読む"){
+			$(this).children('span').text("閉じる");
+			$(this).css({'background':'none'});
+			$(this).css({'position':'static'});
+		}
+		else{
+			$(this).children('span').text("続きを読む");
+			$(this).css({'position':'absolute'});
+			$(this).css({'background':'rgba(0, 0, 0, 0) linear-gradient(180deg, rgba(255, 255, 255, 0.5), white) repeat scroll 0 0'});
+		}
 	});
 });
